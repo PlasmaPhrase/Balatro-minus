0cfbd079903fcc45453f70cfdbe4095a9f4a7ccf
Plasma <plasmaphrase@gmail.com>
2026-03-02 20:50:33 +0000

    initial comit

---
+++ decks/bear_deck.lua
@@ -0,0 +1,20 @@
+
+SMODS.Back {
+    key = 'bear_deck',
+    pos = { x = 0, y = 0 },
+    config = {
+    },
+    loc_txt = {
+        name = 'Bear Deck',
+        text = {
+            [1] = 'Start run with {C:attention}Overstock{}',
+            [2] = '{C:purple}Collectible{} Jokers {C:blue}will {}spawn'
+        },
+    },
+    unlocked = true,
+    discovered = true,
+    no_collection = false,
+    atlas = 'CustomDecks',
+    apply = function(self, back)
+    end
+}
